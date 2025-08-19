b5a1dc09804a1b55cd877d9bdba3cd69f2815101
fallen02 <000.sb13@gmail.com>
2025-08-19 21:14:27 +0530

    gtag-3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const location = useLocation()
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-JDEDNS1RVY');
+  })
   return (
     <>
     <Header />
--- app/root.tsx
+++ app/root.tsx
@@ -46,6 +46,7 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Catamaran:wght@100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap",
   },
+
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -59,13 +60,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {/* <!-- Google tag (gtag.js) --> */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-JDEDNS1RVY">
         </script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-JDEDNS1RVY');
-        </script>
+        
 
       </head>
       <body>
